814b368df0cf85e721fcd686cbddd46f63c734f8
Anastasia <ahinata1717@gmail.com>
2025-11-03 17:41:14 +0200

    Implemented notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,42 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const container = document.createElement('div');
+
+  container.classList.add('notification');
+
+  switch (type) {
+    case 'success':
+      container.classList.add('success');
+      break;
+    case 'error':
+      container.classList.add('error');
+      break;
+    case 'warning':
+      container.classList.add('warning');
+      break;
+  }
+
+  const h2Element = document.createElement('h2');
+
+  h2Element.classList.add('title');
+  h2Element.textContent = title;
+
+  const pElement = document.createElement('p');
+
+  pElement.textContent = description;
+  container.appendChild(h2Element);
+  container.appendChild(pElement);
+
+  container.style.position = 'absolute';
+  container.style.top = `${Number(posTop)}px`;
+  container.style.right = `${Number(posRight)}px`;
+
+  document.body.appendChild(container);
+
+  setTimeout(() => {
+    container.style.visibility = 'hidden';
+  }, 2000);
 };
 
 pushNotification(
